4d3fd011ac93f54b9ff470e896cbe46a8e70679b
Alison <alisonquato@gmail.com>
2023-03-03 23:23:55 -0500

    assets

---
+++ Origine/Assets/scripts/DialogStart.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogStart : MonoBehaviour
+{
+    public GameObject dialog;
+    public GameObject graph;
+    public GameObject removeGraph;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        dialog.SetActive (true);
+        gameObject.SetActive(false);
+        if(graph != null)
+        {
+            graph.SetActive(true);
+        }
+
+        if(removeGraph != null)
+        {
+            removeGraph.SetActive(false);
+        }
+        
+    }
+
+}
